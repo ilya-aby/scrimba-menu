7290c476bfae4f4c3536ab38bf76f5a07f04bde8
ilya-aby <2776078+ilya-aby@users.noreply.github.com>
2024-09-25 12:16:54 -0700

    Adding working payment modal & confirmation

--- main.js
+++ main.js
@@ -15,6 +15,9 @@ document.addEventListener('click', (e) => {
     order.splice(index, 1);
     renderOrder(order);
   }
+  if (e.target.classList.contains('order-complete-button')) {
+    showPaymentModal();
+  }
 });
 
 function renderMenu(menu) {
@@ -63,11 +66,46 @@ function renderOrder(order) {
     <p class="order-total-label">Total price:</p>
     <p class="order-total-price">$${order.reduce((total, item) => total + item.price, 0)}</p>
   </div>
-  <button class="order-total-button">Complete order</button>
+  <button class="order-complete-button">Complete order</button>
 
   `;
 
   orderContainer.innerHTML = orderHeaderHtml + orderHtml + orderFooterHtml;
+}
+
+function showPaymentModal() {
+  const paymentModal = document.createElement('div');
+  paymentModal.classList.add('payment-modal');
+  paymentModal.innerHTML = `
+  <div class="payment-modal-content">
+    <p class="payment-modal-title">Enter card details</p>
+    <form class="payment-modal-form">
+      <input type="text" id="name" name="name" placeholder="Enter your name" required>
+      <input type="number" id="card-number" name="card-number" placeholder="Enter card number" required>
+      <input type="number" id="cvv" name="cvv" placeholder="Enter CVV" required>
+			<button class="payment-modal-button" type="submit">Pay</button>
+    </form>
+  `;
+  document.body.appendChild(paymentModal);
+  paymentModal.addEventListener('submit', function(e){
+    e.preventDefault()
+    console.log('Payment modal submitted');
+    document.body.removeChild(paymentModal);
+
+    // Reset the order on successful payment
+    order = [];
+    renderOrder(order);
+
+    showConfirmationMessage();
+  })
+}
+
+function showConfirmationMessage() {
+  const confirmationMessage = document.querySelector('.confirmation-message');
+  confirmationMessage.style.display = 'block';
+  setTimeout(() => {
+    confirmationMessage.style.display = 'none';
+  }, 5000); // Hide the message after 3 seconds
 }
 
 renderMenu(menu);
